35362d54396db166ed6fc2f494f33ca47124c1d4
root <root@localhost.localdomain>
2022-11-19 16:19:40 +0800

    add test.sh

---
+++ scripts/test_bdd100k.sh
@@ -0,0 +1,26 @@
+#python3 setup.py build develop #--no-deps
+#python3 setup.py develop #--no-deps
+
+export DETECTRON2_DATASETS=datasets
+export DETECTRON2_OUTPUTS=outputs/train/bdd100k
+#export CUDA_LAUNCH_BLOCKING=1 # for debug
+
+ID=159
+
+CUDA_VISIBLE_DEVICES=5,6 python projects/BoundaryFormer/train_net.py --num-gpus 2\
+  --config-file projects/BoundaryFormer/configs/COCO-InstanceSegmentation/boundaryformer_rcnn_R_50_FPN_1x.yaml \
+  --eval-only MODEL.WEIGHTS model/cityscapes_r50_fpn_1x_coco.pth
+
+# mkdir -p datasets/coco
+# ln -s /path_to_coco_dataset/annotations datasets/coco/annotations
+# ln -s /path_to_coco_dataset/train2017 datasets/coco/train2017
+# ln -s /path_to_coco_dataset/test2017 datasets/coco/test2017
+# ln -s /path_to_coco_dataset/val2017 datasets/coco/val2017
+
+# ln -s /data/home/ljl/Datasets/ch_poc/bdd100+/bdd100k_ins_seg_labels_trainval/bdd100k/labels/ins_seg/coco_annotations datasets/coco/annotations
+# ln -s /data/home/ljl/Datasets/ch_poc/bdd100+/bdd100k_images_100k/bdd100k/images/10k/train datasets/coco/train2017
+# ln -s /data/home/ljl/Datasets/ch_poc/bdd100+/bdd100k_images_100k/bdd100k/images/10k/test datasets/coco/test2017
+# ln -s /data/home/ljl/Datasets/ch_poc/bdd100+/bdd100k_images_100k/bdd100k/images/10k/val datasets/coco/val2017
+
+
+
---
+++ scripts/test_cityscapes.sh
@@ -0,0 +1,19 @@
+#python3 setup.py build develop #--no-deps
+#python3 setup.py develop #--no-deps
+
+export DETECTRON2_DATASETS=/data/home/ljl/Datasets/ch_poc
+export DETECTRON2_OUTPUTS=outputs/
+#export CUDA_LAUNCH_BLOCKING=1 # for debug
+
+ID=159
+
+CUDA_VISIBLE_DEVICES=0,1 python projects/BoundaryFormer/train_net.py --num-gpus 2\
+  --config-file projects/BoundaryFormer/configs/Cityscapes/boundaryformer_rcnn_R_50_FPN_1x.yaml \
+  --eval-only MODEL.WEIGHTS model/cityscapes_r50_fpn_1x_coco.pth
+
+# CUDA_VISIBLE_DEVICES=0,1 python3 tools/train_net_swinb.py --num-gpus 2 --resume --dist-url tcp://0.0.0.0:12346 \
+# 	--config-file projects/BoundaryFormer/configs/Cityscapes/boundaryformer_rcnn_R_50_FPN_1x.yaml \
+#         --eval-only MODEL.WEIGHTS model/cityscapes_r50_fpn_1x_coco.pth
+
+
+
